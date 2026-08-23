31ddca22119cda28fc375aeab74ad9e31e16eec6
Techxiaomi <144563117+Techxiaomi@users.noreply.github.com>
2026-08-23 19:16:34 -0300

    revert: remove misplaced BacBo collector integration

--- robo-bacbo/start.js
+++ robo-bacbo/start.js
@@ -3,10 +3,5 @@
 const path = require('path');
 
 require('./env_loader').loadEnvFile(path.join(__dirname, '..', '.env'));
-const redisRuntime = require('./redis_runtime_v3');
-redisRuntime.instalarRedisRuntimeV3();
+require('./redis_runtime_v3').instalarRedisRuntimeV3();
 require('./bot2_coletor');
-
-void require('./bacbo_startup_sync')
-    .sincronizarSnapshotRetido(redisRuntime.processarBacbo)
-    .catch(erro => console.warn(`⚠️ Startup ROAD inesperadamente falhou: ${erro.message}`));
