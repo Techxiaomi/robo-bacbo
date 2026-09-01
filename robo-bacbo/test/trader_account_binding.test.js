d7d8c2ef78f1a184939bdbb5337f2821973bd416
Techxiaomi <144563117+Techxiaomi@users.noreply.github.com>
2026-09-01 20:18:49 -0300

    test(auto-trader): cover config account synchronization

--- robo-bacbo/test/trader_account_binding.test.js
+++ robo-bacbo/test/trader_account_binding.test.js
@@ -6,11 +6,20 @@ const assert = require('node:assert/strict');
 const {
     normalizeAccountIds,
     liveBridgeAdapterSupported,
+    configWithAccountIds,
     SUPPORTED_LIVE_BRIDGE_ADAPTERS
 } = require('../trader_account_binding');
 
 test('normaliza IDs de contas sem duplicatas e em ordem', () => {
     assert.deepEqual(normalizeAccountIds(['4', 1, '4', 1]), [1, 4]);
+});
+
+test('sincroniza account_ids sem destruir configuracao financeira existente', () => {
+    assert.deepEqual(
+        configWithAccountIds('{"stake_inicial":5,"account_ids":[9]}', [4, 1, 4]),
+        { stake_inicial: 5, account_ids: [1, 4] }
+    );
+    assert.deepEqual(configWithAccountIds('{json-invalido', [1]), { account_ids: [1] });
 });
 
 test('vinculo aceita somente adapters com Live Bridge homologado', () => {
