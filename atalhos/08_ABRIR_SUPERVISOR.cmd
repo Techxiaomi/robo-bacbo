6235639119cc947060ef4eb8925358f15eaa3994
Techxiaomi <144563117+Techxiaomi@users.noreply.github.com>
2026-09-02 13:32:53 -0300

    fix(accesses): keep supervisor shortcut single-tab

--- atalhos/08_ABRIR_SUPERVISOR.cmd
+++ atalhos/08_ABRIR_SUPERVISOR.cmd
@@ -1,7 +1,2 @@
 @echo off
-setlocal EnableExtensions
-set "URL=http://127.0.0.1:3010/supervisor"
-call "%~dp092_ACESSOS.cmd" >nul 2>&1
-if errorlevel 1 exit /b %ERRORLEVEL%
-start "" "%URL%"
-exit /b 0
+start "" "http://127.0.0.1:3010/supervisor"
